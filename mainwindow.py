33cdb751b6af0b0fd5f0499e625fab2797a8d01f
Abdul6498 <abdulrehman6498@outlook.com>
2022-12-05 12:26:46 +0100

    ML and RL code added

--- mainwindow.py
+++ mainwindow.py
@@ -24,6 +24,7 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 import numpy as np
 from threading import *
+from AutoLabel import AutoLabel, read_data
 
 # setting path
 sys.path.insert(1, '../thesis')
@@ -31,6 +32,7 @@ from scripts.Velocity import move
 from scripts.py_ros_launch import ros_launch_without_core
 import config as config
 from main import mains
+import subprocess
 
 prv_txt = "GUI started"
 Pose_data = [0,0,0]
@@ -61,28 +63,60 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.thread = {}
         self.corners = []
+        self.filename = ""
+
+        self.ERROR = QColor(255, 0, 0)
+        self.NORMAL = QColor(0, 0, 0)
+        self.WARNING = QColor(255,153,102)
+        self.HIGHLIGHT = QColor(255,204,0)
+        self.OK = QColor(51,153,0)
+        self.BLUE = QColor(0,0,255)
+
+        self.ui.info_box.setText("GUI Started")
 
         self.ui.shape_selector.addItem("select shape")
         self.ui.shape_selector.addItem("circle")
         self.ui.shape_selector.addItem("rectangle")
         self.ui.shape_selector.addItem("polygon")
+
+        self.ui.tree_selector.addItem("select tree type")
+        self.ui.tree_selector.addItem("BRUTE_FORCE")
+        self.ui.tree_selector.addItem("BINARY_SEARCH")
+        self.ui.tree_selector.addItem("IDA")
+
+        self.ui.ml_algo_selector.addItem("select algorithm")
+        self.ui.ml_algo_selector.addItem("Kmeans")
+        self.ui.ml_algo_selector.addItem("mixture of gaussian")
+        self.ui.ml_algo_selector.addItem("Agglomerative")
+        self.ui.ml_algo_selector.addItem("Mini batch Kmeans")
+
         self.ui.virtual_obj_btn.setEnabled(False)
         self.ui.close_window_btn.setEnabled(False)
+        self.ui.ML_autolabel.setEnabled(False)
+        self.ui.Test_btn.setEnabled(False)
+
         self.ui.shape_selector.currentIndexChanged.connect(self.onshape_selector_changed)
+        self.ui.tree_selector.currentIndexChanged.connect(self.treeselector_changed)
+        self.ui.ml_algo_selector.currentIndexChanged.connect(self.select_algo)
+
         self.ui.actionOpen_Map.triggered.connect(self.file_open)
         self.ui.actionInfo.triggered.connect(self.showDialog)
         self.ui.save_map_btn.clicked.connect(lambda: self.show_info(self.ui.path_to_map.text(), new_line=True))
         self.ui.speed_dial.valueChanged.connect(self.show_speed)
-        self.ui.info_box.setText("GUI Started")
+
         self.ui.traj_btn.clicked.connect(lambda: self.show_map(self.ui.path_to_map.text()))
         self.ui.start_rob_btn.clicked.connect(lambda: self.start_robot())
-        self.ui.custom_btn_1.clicked.connect(lambda: self.start_worker())
-        self.ui.custom_btn_2.clicked.connect(lambda: self.stop_worker())
-        self.ui.custom_btn_3.clicked.connect(lambda: self.start_worker_2())
-        self.ui.custom_btn_4.clicked.connect(lambda: self.stop_worker_2())
+
+        self.ui.Train.clicked.connect(lambda: self.start_worker(mode=False))
+        self.ui.stop_train_test.clicked.connect(lambda: self.stop_worker())
+        self.ui.Test_btn.clicked.connect(lambda: self.start_worker(mode=True))
+
         self.ui.custom_btn_5.clicked.connect(lambda: self.clicklaunch_())
+
         self.ui.virtual_obj_btn.clicked.connect(lambda: self.create_geofence())
         self.ui.close_window_btn.clicked.connect(lambda: self.destroy_windows())
+
+        self.ui.ML_autolabel.clicked.connect(lambda: self.predict_clusters())
 
     def start_robot(self):
         #t1=Thread(target=self.Operation, daemon=True)
@@ -100,59 +134,113 @@ class MainWindow(QMainWindow):
         print ("Launching...")
         launch_files_v = "/home/abdul/AbdulRehman_ws/src/IDT/real_nav_control/launch"
         generate_map_launch = launch_files_v + "/" + "generate_map.launch"
-        program = "roslaunch " + " " + generate_map_launch
+        program = "roslaunch" + " " + generate_map_launch
         print(program)
-        ROS_PROGRAM.start(program)
-        #os.system(program)
-  
-    def Operation(self):
-        """ 
-        40: complete rotation at 0.15
-        time is calculated from distance and velocity"""
-        # path =        [39.5, 11, 39.5, 11, 39.5,  10,   6, 39.5,  10,  11, 39.5,  11, 39.5,  10,  8,   9,   11,  39,  9, 39] # Time
-        # direction =   ["r", "f",  "r", "f", "r", "r", "f",  "r", "r", "f",  "r", "f",  "r", "r", "f", "r", "f", "r", "f", "r"]   # Move forward or rotation
-        # orientation = [True, -1, True,  -1,True,True,  -1, True,True,  -1, True,  -1,  True,False,-1,False, -1,True,  -1, True]
-
-        #ros_launch_without_core()
-        pass
+        ROS_PROGRAM.start("roslaunch", [generate_map_launch])
         
+        # cmd = ['rosrun', 'map_server', 'map_saver', 'map:=dynamic_map', '-f', 'test_map']
+        # subprocess.call(cmd) == 0
+        #os.system(program)
+
+    def select_algo(self):
+        algo_type = self.ui.ml_algo_selector.currentText()
+        if algo_type == "select algorithm":
+            #print("Select circle, rectangle, or polygon")
+            self.ui.info_box_ml.setText("Select Kmeans or Mixture of Gaussians")
+            self.ui.ML_autolabel.setEnabled(False)
+        else:
+            self.ui.info_box_ml.append(algo_type)    
+            self.ui.ML_autolabel.setEnabled(True)
+
+    def treeselector_changed(self):
+        tree_type = self.ui.tree_selector.currentText()
+        if tree_type == "select tree type":
+            #print("Select circle, rectangle, or polygon")
+            self.ui.info_box_rl.setText("Select BRUTE_FORCE or IDA")
+            self.ui.Test_btn.setEnabled(False)
+        else:
+            self.ui.info_box_rl.setText(tree_type)    
+            self.ui.Test_btn.setEnabled(True)
 
     def onshape_selector_changed(self):
         object_type = self.ui.shape_selector.currentText()
         if object_type == "select shape":
-            print("Select circle, rectangle, or polygon")
+            #print("Select circle, rectangle, or polygon")
+            object_type
             self.ui.virtual_obj_btn.setEnabled(False)
             self.ui.close_window_btn.setEnabled(False)
         else:    
+            self.ui.info_box_rl.append(object_type)  
             self.ui.virtual_obj_btn.setEnabled(True)
             self.ui.close_window_btn.setEnabled(True)
-    def start_worker(self):
-        self.thread[1] = ThreadClass(parent=None, index=1)
+
+    def print_ml(self, text, color=QColor(0, 0, 0), append=False):
+        self.ui.info_box_ml.setTextColor(color)
+        if append:
+            self.ui.info_box_ml.append(text)
+        else:
+            self.ui.info_box_ml.setText(text)
+        self.ui.info_box_ml.setTextColor(color)
+
+    def print_rl(self, text, color=QColor(0, 0, 0), append=False):
+        self.ui.info_box_rl.setTextColor(color)
+        if append:
+            self.ui.info_box_rl.append(text)
+        else:
+            self.ui.info_box_rl.setText(text)
+        self.ui.info_box_rl.setTextColor(self.NORMAL)
+
+    def predict_clusters(self):
+        algo_type = self.ui.ml_algo_selector.currentText()
+        if len(self.filename) < 10:
+            self.print_ml("Please select the collision_data.csv",append=True, color=self.WARNING)
+        elif self.filename.split(".")[-1] != "csv":
+            self.print_ml("Please select the appropriate file",append=True, color=self.WARNING)
+            
+        else:
+            self.ui.info_box_ml.append("ML Started")
+            data, df = read_data(self.filename)
+            al = AutoLabel(data, n_clusters = 3)
+            if algo_type == "Kmeans":
+                al.K_Means()
+            if algo_type == "mixture of gaussian":
+                al.MixtureofGaussians()
+            if algo_type == "Agglomerative":
+                al.agglomerative()
+            if algo_type == "Mini batch Kmeans":
+                al.MiniBatch_KMeans()
+        
+    def train_test(self, risk=-1):
+        #print("Test mode: ")
+        indexs = self.sender().index
+        print("RISK in GUI:",risk)
+        self.print_rl("RISK:"+ str(risk), color = self.ERROR)
+        if risk >=0:
+            #print(self.thread[1].is_running)
+            #print(indexs)
+            self.ui.Train.setEnabled(True)
+            self.ui.Test_btn.setEnabled(True)
+            self.stop_worker()
+
+    def start_worker(self, mode=False):
+        #mcts_eval = "BRUTE_FORCE"
+        mcts_eval = self.ui.tree_selector.currentText()
+        #print(mode)
+        self.thread[1] = ThreadClass(parent=None, mode=mode,mcts_eval=mcts_eval, index=1)
         self.thread[1].start()
-        self.thread[1].any_signal.connect(self.testing_fun)
-        self.ui.custom_btn_1.setEnabled(False)
+        self.thread[1].any_signal.connect(self.train_test)
+        self.ui.Train.setEnabled(False)
+        self.ui.Test_btn.setEnabled(False)
     
     def stop_worker(self):
-        self.thread[1].stop()
-        self.ui.custom_btn_1.setEnabled(True)
-
-    def start_worker_2(self):
-        self.thread[2] = ThreadClass(parent=None, index=2)
-        self.thread[2].start()
-        self.thread[2].any_signal.connect(self.testing_fun)
-        self.ui.custom_btn_3.setEnabled(False)
-    
-    def stop_worker_2(self):
-        self.thread[2].stop()
-        self.ui.custom_btn_3.setEnabled(True)
-
-    def testing_fun(self,counter):
-        cnt=counter
-        index = self.sender().index
-        if index==1:
-            self.ui.speed_real.display(cnt)
-        if index==2:
-            self.ui.speed_lcd.display(cnt)
+        #print(self.thread[1].is_running, indexs)
+        #print(self.thread[2].is_running, indexs)
+        if self.thread[1].is_running==False:
+            self.thread[1].stop()
+        # if self.thread[2].is_running==False:
+        #     self.thread[2].stop()
+        self.ui.Train.setEnabled(True)
+        self.ui.Test_btn.setEnabled(True)
 
     # Show all info in the info box
     def show_info(self, text, new_line = True):
@@ -174,17 +262,19 @@ class MainWindow(QMainWindow):
             scene.addItem(item)
             self.ui.map_display.setScene(scene)
 
-    def file_open(self):
+    def file_open(self, map=True):
         name = QFileDialog.getOpenFileName(self)
-        self.show_map(name[0])
+        if map:
+            self.show_map(name[0])
+        self.filename = name[0]
+        #return name[0]
         #file = open(name, 'r')
         #print(name[0])
         
     def showDialog(self):
         msgBox = QMessageBox()
         msgBox.setIcon(QMessageBox.Information)
-        msgBox.setText("Created by Abdul Rehman \n \
-                        Under development")
+        msgBox.setText("Created by Abdul Rehman \n Under development")
         msgBox.setWindowTitle("Information")
         msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
 
@@ -203,7 +293,10 @@ class MainWindow(QMainWindow):
             points = [x,y]
             self.corners.append(points)
             print(x, ' ', y)
-            print(flags)
+            #print(flags)
+            self.ui.info_box_rl.append(str(x) + ' ' + str(y))
+            #self.ui.info_box_rl.setText(str(flags))
+            
             # displaying the coordinates
             # on the image window
             font = cv2.LINE_AA
@@ -223,6 +316,7 @@ class MainWindow(QMainWindow):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         print(self.corners)
+        self.ui.info_box_rl.append(str(self.corners))
         pts = np.array(self.corners)
         pts = pts.reshape((-1, 1, 2))
 
@@ -264,6 +358,8 @@ class MainWindow(QMainWindow):
         # Red color in BGR
         color = (0, 0, 0)
         print(object)
+        self.ui.info_box_rl.append(object)
+        
         if object == "rectangle":
             image = cv2.rectangle(img, start_point, end_point, color, thickness)
         else:
@@ -318,51 +414,28 @@ class MainWindow(QMainWindow):
         #move()
         
         #rospy.spin()    ##Run the node until we shutdown
-    
-
-
-    # def move_forward(self, text):
-    #     self.Speed.setText(text)
-    #     self.Speed.adjustSize()
-    #     move_publisher = rospy.Publisher('cmd_vel_real', Twist, queue_size=10)
-    #     move_publisher.publish(drive_forward())
-            
-    # def move_backward(self, text):
-    #     self.Speed.setText(text)
-    #     self.Speed.adjustSize()
-    
-    # def move_right(self, text):
-    #     self.Speed.setText(text)
-    #     self.Speed.adjustSize()
-    
-    # def move_left(self, text):
-    #     self.Speed.setText(text)
-    #     self.Speed.adjustSize()
-    
-    # def stop(self, text):
-    #     self.Speed.setText(text)
-    #     self.Speed.adjustSize()
-    #     move_publisher = rospy.Publisher('cmd_vel_real', Twist, queue_size=10)
-    #     move_publisher.publish(stop_robot())
 
 class ThreadClass(QThread):
-    any_signal = Signal(int)
-    def __init__(self, parent=None, index=0):
+    any_signal = Signal(float)
+    def __init__(self, parent=None, mode=False, mcts_eval="BRUTE_FORCE", index=0):
         super(ThreadClass,self).__init__(parent)
-        self.index = index
+        self.mode = mode
         self.is_running = True
+        self.index = index
+        self.mcts = mcts_eval
     
     def run(self):
-        print('Starting Thread...', self.index)
-        cnt=0
-        while (True):
-            cnt+=1
-            if cnt==99: cnt=0
-            time.sleep(0.1)
-            self.any_signal.emit(cnt)
+        print('Starting Thread...', self.mode)
+        #cnt=0
+        done, risk = mains(mode=self.mode, mcts_eval=self.mcts)
+        self.is_running=False
+        if done:
+            self.any_signal.emit(risk)
+        # while (True):
+        #     self.any_signal.emit(done)
     def stop(self):
         self.is_running=False
-        print('Stopping thread...', self.index)
+        print('Stopping thread... ', self.mode)
         self.terminate()
     
 if __name__ == "__main__":
