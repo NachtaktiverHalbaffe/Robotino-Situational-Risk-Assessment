09b19988eea5f6f6c7f92b1a05874d8e5274cead
Abdul6498 <abdulrehman6498@outlook.com>
2022-12-12 13:05:52 +0100

    minor changes

--- plots.py
+++ plots.py
@@ -3,7 +3,7 @@ import numpy as np
 import evaluate_logs
 
 from CSV_Logger import CSV_Reader
-avg_window = 500   # determines how many episodes are averaged in plots
+avg_window = 1000   # determines how many episodes are averaged in plots
 
 # log_reader = CSV_Reader('prot_1', test_mode=False)
 # evaluate_logs.plot_collisions('prot_1', log_reader, avg_window=avg_window)
@@ -12,7 +12,7 @@ avg_window = 500   # determines how many episodes are averaged in plots
 print('probs adv:')
 #evaluate_logs.calc_probabilities(log_reader)  # for evaluation: calculates and prints the frequencies of chosen actions (for histogram)
 
-log_reader = CSV_Reader('adv_1', test_mode=True)
+log_reader = CSV_Reader('adv_1', test_mode=False)
 evaluate_logs.plot_rewards('adv_1', log_reader, avg_window=3*avg_window)    # rewards (for adversary) are per step instead of per episode -> choose bigger average window
 evaluate_logs.plot_scores('adv_1', log_reader, avg_window=avg_window)
 evaluate_logs.plot_collisions('adv_1', log_reader, avg_window=avg_window)
