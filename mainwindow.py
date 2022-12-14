d5f06bcd533566cafdcfe6c9a62f4c1f0fac64bd
Abdul6498 <abdulrehman6498@outlook.com>
2022-12-14 18:58:13 +0100

    evaluate function added

--- mainwindow.py
+++ mainwindow.py
@@ -666,7 +666,7 @@ class MainWindow(QMainWindow):
         """
         object_type = self.ui.shape_selector.currentText()
         configs = config.configs[0]
-        map = cv2.imread(configs["map_path"], cv2.IMREAD_GRAYSCALE)
+        map = cv2.imread(configs["map_path"])#, cv2.IMREAD_GRAYSCALE)
 
         scale_percent = 200  # percent of original size
         width = int(map.shape[1] * scale_percent / 100)
